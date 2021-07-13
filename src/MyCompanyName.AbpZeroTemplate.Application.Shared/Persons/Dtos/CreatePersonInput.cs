59b0824f4711ce654cdc72fd8d9b743c935f6d8c
datdd.bdhn <datdd.bdhn@gmail.com>
2021-07-13 09:07:28 +0700

    crud devexpress

---
+++ src/MyCompanyName.AbpZeroTemplate.Application.Shared/Persons/Dtos/CreatePersonInput.cs
@@ -0,0 +1,21 @@
+﻿using Abp.Application.Services.Dto;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace MyCompanyName.AbpZeroTemplate.Persons.Dtos
+{
+    public class CreatePersonInput
+    {
+        [Required]
+        [MaxLength(PersonConsts.MaxNameLength)]
+        public string Name { get; set; }
+
+        [Required]
+        [MaxLength(PersonConsts.MaxSurnameLength)]
+        public string Surname { get; set; }
+
+        [EmailAddress]
+        [MaxLength(PersonConsts.MaxEmailAddressLength)]
+        public string EmailAddress { get; set; }
+    }
+}
---
+++ src/MyCompanyName.AbpZeroTemplate.Application.Shared/Persons/Dtos/EditPersonInput.cs
@@ -0,0 +1,15 @@
+﻿using Abp.Application.Services.Dto;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace MyCompanyName.AbpZeroTemplate.Persons.Dtos
+{
+    public class EditPersonInput : EntityDto
+    {
+        public string Name { get; set; }
+
+        public string Surname { get; set; }
+
+        public string EmailAddress { get; set; }
+    }
+}
