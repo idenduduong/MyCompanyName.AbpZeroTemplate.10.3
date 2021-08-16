90a8471d0e7af18e8d6692628230d8ca076da4b5
invisible <invisible1234everyone@gmail.com>
2021-08-16 11:53:56 +0700

    UPDATE TO 10.4.1

--- src/MyCompanyName.AbpZeroTemplate.Application/Authorization/Users/UserLoginAppService.cs
+++ src/MyCompanyName.AbpZeroTemplate.Application/Authorization/Users/UserLoginAppService.cs
@@ -38,5 +38,31 @@ namespace MyCompanyName.AbpZeroTemplate.Authorization.Users
 
             return new ListResultDto<UserLoginAttemptDto>(ObjectMapper.Map<List<UserLoginAttemptDto>>(loginAttempts));
         }
+
+
+        public async Task<PagedResultDto<UserLoginAttemptDto>> GetUserLoginAttempts(GetLoginAttemptsInput input)
+        {
+            var userId = AbpSession.GetUserId();
+            var query = _userLoginAttemptRepository.GetAll()
+                .Where(la => la.UserId == userId)
+                .WhereIf(!input.Filter.IsNullOrEmpty(), la => la.ClientIpAddress.Contains(input.Filter) || la.BrowserInfo.Contains(input.Filter))
+                .WhereIf(input.StartDate.HasValue, la => la.CreationTime >= input.StartDate)
+                .WhereIf(input.EndDate.HasValue, la => la.CreationTime <= input.EndDate)
+                .WhereIf(input.Result.HasValue, la => la.Result == input.Result);
+
+            var loginAttemptCount = await query.CountAsync();
+
+            var loginAttempts = await query
+                .OrderBy(input.Sorting)
+                .PageBy(input)
+                .ToListAsync();
+
+            var loginAttemptDtos = ObjectMapper.Map<List<UserLoginAttemptDto>>(loginAttempts);
+
+            return new PagedResultDto<UserLoginAttemptDto>(
+                loginAttemptCount,
+                loginAttemptDtos
+            );
+        }
     }
 }
